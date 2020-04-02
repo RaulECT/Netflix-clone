b3c82c6d7ec62308a1b238f5ae918f59408f6bab
Raul Canul <eduaroct120@gmail.com>
2020-04-01 20:47:42 -0600

    Movies schema created

---
+++ src/models/Movies.js
@@ -0,0 +1,57 @@
+const mongoose = require( 'mongoose' );
+
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    image_cover_uri: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: [ 'Action', 'Romance', 'Comedy' ]
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    video_uri: {
+        type: String,
+        required: true
+    },
+    trailer_uri: {
+        type: String,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    is_avaible: {
+        type: Boolean,
+        default: true
+    },
+    liked_by: {
+        type: [Schema.Types.ObjectId],
+        ref: 'users'
+    }
+});
+
+module.exports = mongoose.model('movies', MovieSchema );
