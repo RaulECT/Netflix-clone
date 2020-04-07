4c7152dfcb436c7aa4bed8f1b7d9e4408789ad26
Raul Canul <eduaroct120@gmail.com>
2020-04-06 20:46:00 -0500

    updateMovie mutation created

--- src/resolvers/MovieResolvers/Mutation.js
+++ src/resolvers/MovieResolvers/Mutation.js
@@ -1,10 +1,16 @@
-const { createOneMovie } = require( '../../services/MovieService' );
+const { createOneMovie, updateById } = require( '../../services/MovieService' );
 
 const createMovie = async (_, { data }) => {
     const movie = await createOneMovie( data );
     return movie;
 };
 
+const updateMovie = async (_, { data, id }) => {
+    const movie = await updateById( id, data );
+    return movie;
+};
+
 module.exports = {
-    createMovie
+    createMovie,
+    updateMovie,
 };
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -3,9 +3,11 @@ const Movies = require('../models/Movies');
 const getAllMovies = () => Movies.find({ is_avaible: true });
 const getMovieByProperties = props => Movies.find({ ...props, is_avaible: true });
 const createOneMovie = data => Movies.create( data );
+const updateById = ( _id, data ) => Movies.findByIdAndUpdate( { _id, is_avaible: true }, { ...data }, { new: true } );
 
 module.exports = {
     createOneMovie,
     getAllMovies,
-    getMovieByProperties
+    getMovieByProperties,
+    updateById,
 };
