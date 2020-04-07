4aa6489d186df2ae7518475994d7586999f78c5e
Raul Canul <eduaroct120@gmail.com>
2020-04-06 21:11:11 -0500

    updateEpisode mutation created

--- src/resolvers/EpisodeResolvers/Mutation.js
+++ src/resolvers/EpisodeResolvers/Mutation.js
@@ -1,10 +1,16 @@
-const { createOneEpisode } = require( '../../services/EpisodeService' );
+const { createOneEpisode, updateById } = require( '../../services/EpisodeService' );
 
 const createEpisode = async (_, { data }) => {
     const episode = await createOneEpisode( data );
     return episode;
 };
 
+const updateEpisode = async (_, { id, data }) => {
+    const episode = await updateById( id, data );
+    return episode;
+};
+
 module.exports = {
     createEpisode,
+    updateEpisode,
 };
--- src/services/EpisodeService.js
+++ src/services/EpisodeService.js
@@ -4,8 +4,12 @@ const createOneEpisode = data => Episode.create( data );
 const getOneEpisodeById = _id => Episode
     .findById({ _id })
     .populate({ path: 'serie', model: 'series' });
+const updateById = ( _id, data ) => Episode
+    .findByIdAndUpdate({ _id }, { ...data }, { new: true })
+    .populate({ path: 'serie', model: 'series' });
 
 module.exports = {
     createOneEpisode,
     getOneEpisodeById,
+    updateById,
 };
