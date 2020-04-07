b5cf0fc72db8d9da0ea8213fbe2c02c0d7beec0e
Raul Canul <eduaroct120@gmail.com>
2020-04-06 20:57:26 -0500

    updateSerie mutation implemented

--- src/resolvers/SerieResolvers/Mutation.js
+++ src/resolvers/SerieResolvers/Mutation.js
@@ -1,10 +1,17 @@
-const { createOneSerie } = require('../../services/SerieService');
+const { createOneSerie, updateById } = require('../../services/SerieService');
 
 const createSerie = async (_, { data }) => {
     const serie = await createOneSerie( data );
     return serie;
 };
 
+const updateSerie = async (_, { data, id }) => {
+    const serie = await updateById( id, data );
+    console.log(serie);
+    return serie;
+};
+
 module.exports = {
-    createSerie
+    createSerie,
+    updateSerie,
 };
--- src/services/SerieService.js
+++ src/services/SerieService.js
@@ -1,13 +1,17 @@
 const Series = require('../models/Series');
 
 const getAllSeries = () => Series
-    .find({ is_avaible: true });
-    // .populate({ path: 'episodes', model: 'episodes' });
+    .find({ is_avaible: true })
+    .populate({ path: 'episodes', model: 'episodes' });
 const getSerieByProperties = props => Series.find( { ...props, is_avaible: true } );
 const createOneSerie = data => Series.create( data );
+const updateById = ( _id, data ) => Series
+    .findByIdAndUpdate( { _id, is_avaible: true }, { ...data }, { new: true } )
+    .populate( { path: 'episodes', model: 'episodes' } );
 
 module.exports = {
     getAllSeries,
     getSerieByProperties,
-    createOneSerie
+    createOneSerie,
+    updateById,
 };
