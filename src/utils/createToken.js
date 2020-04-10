35060088abefe8b9b0019b772580100fbad1a24a
Raul Canul <eduaroct120@gmail.com>
2020-04-09 20:34:21 -0500

    createToken function implemented

---
+++ src/utils/createToken.js
@@ -0,0 +1,9 @@
+const jwt = require( 'jsonwebtoken' );
+
+const createToken = ({ email, first_name }) => {
+    const payload = { email, first_name };
+
+    return jwt.sign( payload, process.env.SECRET_KEY_JWT );
+};
+
+module.exports = createToken;
