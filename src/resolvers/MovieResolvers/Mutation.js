81084bebf12e579a9d64b78d35043164155562dd
Raul Canul <eduaroct120@gmail.com>
2020-04-13 19:58:30 -0500

    upload file implemented on Movie mutations

--- src/resolvers/MovieResolvers/Mutation.js
+++ src/resolvers/MovieResolvers/Mutation.js
@@ -1,12 +1,32 @@
 const { createOneMovie, updateById, deleteById, getMovieById } = require( '../../services/MovieService' );
+const storage = require( '../../utils/storage' );
 
 const createMovie = async (_, { data }) => {
-    const movie = await createOneMovie( data );
+    const movieCover = await uploadFile( data.image_cover_uri );
+    const movieVideo = await uploadFile( data.video_uri, true );
+    const movieTrailer = await uploadFile( data.trailer_uri, true );
+
+    const movie = await createOneMovie( {
+        ...data,
+        image_cover_uri: movieCover,
+        video_uri: movieVideo,
+        trailer_uri: movieTrailer
+    } );
+
     return movie;
 };
 
 const updateMovie = async (_, { data, id }) => {
-    const movie = await updateById( id, data );
+    const movieCover = await uploadFile( data.image_cover_uri );
+    const movieVideo = await uploadFile( data.video_uri, true );
+    const movieTrailer = await uploadFile( data.trailer_uri, true );
+
+    const movie = await updateById( id, {
+        ...data,
+        image_cover_uri: movieCover,
+        video_uri: movieVideo,
+        trailer_uri: movieTrailer
+    } );
     return movie;
 };
 
@@ -22,6 +42,14 @@ const rateMovie = async (_, { movie_id, user_id, rate }) => {
     const movieUpdated = await updateById( movie_id, { liked_by: movieLikes, rating: movieRate } );
 
     return movieUpdated;
+};
+
+const uploadFile = async (file, isVideo = false) => {
+    const { createReadStream } = await file;
+    const stream = createReadStream();
+    const storageInfo = await storage({ stream }, isVideo);
+    console.log( 'Storage info', storageInfo);
+    return storageInfo.secure_url;
 };
 
 module.exports = {
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,21 +1,20 @@
-const cloudinary = require( 'cloudinary' );
+const cloudinary = require('cloudinary');
 
-const storage = ({ stream }) => {
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
+const storage = ({stream}, isVideo = false) => {
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET
     });
-
-    return new Promise( (resolve, reject) => {
-        const buffer = cloudinary.v2.uploader.upload_stream( ( err, result ) => {
-            if( err ) reject( err );
-
-            resolve( result );
-        } );
-
-        stream.pipe( buffer );
-    } );
+    // { image: información } fregf459fi34f9430ir
+    return new Promise((resolve, reject) => {
+        const options = isVideo ? { resource_type: 'video' } : {};
+        const buffer = cloudinary.v2.uploader.upload_stream( options, (err, result) => {
+            if(err) reject(err);
+            resolve(result);
+        });
+        stream.pipe(buffer);
+    });
 };
 
 module.exports = storage;
